22ff7a627fbc9bb716769b377dc9e576eeef3211
Javier Cabrera <javierca@kth.se>
2019-09-04 11:16:14 +0200

    WIP

--- runtime/src/swam/runtime/internals/tracer/StdTracer.scala
+++ runtime/src/swam/runtime/internals/tracer/StdTracer.scala
@@ -8,7 +8,7 @@ class StdTracer extends Tracer{
     def group(args: Any*) = args.mkString(",")
 
     def traceEvent(args: Any*) = {
-        super.executeOnBack(()=> println(s"${Console.MAGENTA}${group(args: _*)},${System.currentTimeMillis()}${Console.WHITE}"))
+        super.executeOnBack(()=> println(s"${Console.MAGENTA} ${group(args: _*)},${System.currentTimeMillis()} ${Console.WHITE}"))
     }
 }
 
