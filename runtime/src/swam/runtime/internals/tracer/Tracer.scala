53f0a438816272640ba9a5368100b04b380778a8
Javier Cabrera <javierca@kth.se>
2019-09-06 15:49:16 +0200

    Reformating

--- runtime/src/swam/runtime/internals/tracer/Tracer.scala
+++ runtime/src/swam/runtime/internals/tracer/Tracer.scala
@@ -34,52 +34,51 @@ class Tracer(val conf: TraceConfiguration) {
     */
   var traceEvent: (String, Any*) => Unit = (name, args) => {}
 
-  class PureFormatter extends Formatter{
+  class PureFormatter extends Formatter {
 
-      override def format(x: LogRecord): String = {
-        return s"${x.getMillis()},${x.getMessage()}\n"
-      }
+    override def format(x: LogRecord): String = {
+      return s"${x.getMillis()},${x.getMessage()}\n"
+    }
   }
 
   def group(args: Any*) = args.mkString(",")
 
-    {
-        loggerImpl
-    }
-  object loggerImpl{
+  {
+    loggerImpl
+  }
+  object loggerImpl {
     if (conf.handler != HandlerType.None) {
-        val logger = Logger.getLogger("swam")
-        logger.setLevel(Level.parse(conf.level))
-        
-        var handler: Handler = null
-    
-        conf.handler match {
-            case HandlerType.Console => {
-                handler = new ConsoleHandler()
-            }
-            case  HandlerType.File => {
-                handler = new FileHandler(s"${conf.fileHandler.folder}/${conf.fileHandler.pattern}",
-                conf.fileHandler.append)
-            }
-            case  HandlerType.Socket => {
-                handler = new SocketHandler(conf.socketHandler.host,
-                conf.socketHandler.port)
-            }
-            case  HandlerType.Custom => handler = Class.forName(conf.custom.className).newInstance().asInstanceOf[java.util.logging.Handler]
-            // The class must have an empty constructor
-            // TODO add custom parameters
+      val logger = Logger.getLogger("swam")
+      logger.setLevel(Level.parse(conf.level))
+
+      var handler: Handler = null
+
+      conf.handler match {
+        case HandlerType.Console => {
+          handler = new ConsoleHandler()
         }
-    
-        // TODO add other formatters support
-        handler.setFormatter(new PureFormatter)
-    
-        logger.setUseParentHandlers(false) // Avoid parent handler
-        logger.addHandler(handler)
-    
-        traceEvent = (name, args) => {
-            logger.info(s"${name},${group(args: _*)}")
+        case HandlerType.File => {
+          handler = new FileHandler(s"${conf.fileHandler.folder}/${conf.fileHandler.pattern}", conf.fileHandler.append)
         }
+        case HandlerType.Socket => {
+          handler = new SocketHandler(conf.socketHandler.host, conf.socketHandler.port)
+        }
+        case HandlerType.Custom =>
+          handler = Class.forName(conf.custom.className).newInstance().asInstanceOf[java.util.logging.Handler]
+        // The class must have an empty constructor
+        // TODO add custom parameters
       }
+
+      // TODO add other formatters support
+      handler.setFormatter(new PureFormatter)
+
+      logger.setUseParentHandlers(false) // Avoid parent handler
+      logger.addHandler(handler)
+
+      traceEvent = (name, args) => {
+        logger.info(s"${name},${group(args: _*)}")
+      }
+    }
   }
 
 }
