702a397c3d701c1d1a72c6aacecabd6bbe7f624b
Jian GU <ycaxgjd@gmail.com>
2019-12-15 14:33:42 +0100

    functional modules

---
+++ slumps/src/swam/slumps/internals/Pass.scala
@@ -0,0 +1,6 @@
+// given LHS, invoke Souper to generate RHS
+// https://github.com/google/souper/blob/master/lib/Pass/Pass.cpp
+// https://github.com/google/souper/blob/master/tools/souper-check.cpp
+
+// Optimizer
+// Randomizer
