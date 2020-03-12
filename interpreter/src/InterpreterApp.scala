9150768b5ca1d2c8337c27bbc5b5c04fe402e4ec
Javier Cabrera <javierca@kth.se>
2020-03-12 23:08:41 +0100

    Starting parameters parsing

--- interpreter/src/InterpreterApp.scala
+++ interpreter/src/InterpreterApp.scala
@@ -108,7 +108,8 @@ object InterpreterApp extends IOApp {
             engine.compile(config.wasm.toPath, blocker, 4096)
 
           instance <- engine.instantiate(module, imports())
-          _ <- instance.exports.function("main")
+          f <- instance.exports.function("main")
+          _ <- f.invoke(Vector(), None)
           exit <- IO(ExitCode.Success)
         } yield exit
       }
