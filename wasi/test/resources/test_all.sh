1692e1c70cad6b351d1872709e624d9500e5ebd0
Javier Cabrera <javierca@kth.se>
2020-04-14 22:39:07 +0200

    Adding tests

--- wasi/test/resources/test_all.sh
+++ wasi/test/resources/test_all.sh
@@ -1,10 +1,22 @@
 pw=wasi/test/resources
 
+echo "Read args"
 mill cli.run $pw/read-args.wasm t1  retyty erte
+
+echo "Merge sort"
 mill cli.run $pw/merge_sort.wasm
+
+echo "Posix"
 mill cli.run $pw/posix.wasm m1.txt 1.txt
+
+echo "Bitwise"
 mill cli.run $pw/bitwise.wasm
-mill cli.run $pw/readdir.wasm
+
+echo "Read dir"
+mill cli.run $pw/readdir.wasm -d .
+
+echo "mkdir"
+mill cli.run $pw/mkdir.wasm
 
 #wasi/test/resources/read-args.wasm t1  retyty erte
 #
