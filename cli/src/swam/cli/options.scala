45cbf18567031888fbc8eb36f2789c840b403657
Javier Cabrera <javierca@kth.se>
2020-06-11 15:24:29 +0200

    Reformatting

--- cli/src/swam/cli/options.scala
+++ cli/src/swam/cli/options.scala
@@ -41,15 +41,16 @@ case class Validate(file: Path, wat: Boolean, dev: Boolean) extends Options
 case class Compile(file: Path, out: Path, debug: Boolean) extends Options
 
 case class WasmCov(file: Path,
-               args: List[String],
-               main: String,
-               wat: Boolean,
-               wasi: Boolean,
-               time: Boolean,
-               trace: Boolean,
-               traceFilter: String,
-               traceFile: Path,
-               dirs: List[Path],
-               debug: Boolean,
-               coverage:Boolean, out: Path)
-  extends Options
+                   args: List[String],
+                   main: String,
+                   wat: Boolean,
+                   wasi: Boolean,
+                   time: Boolean,
+                   trace: Boolean,
+                   traceFilter: String,
+                   traceFile: Path,
+                   dirs: List[Path],
+                   debug: Boolean,
+                   coverage: Boolean,
+                   out: Path)
+    extends Options
