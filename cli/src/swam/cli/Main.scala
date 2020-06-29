6271b7b83bca715cc1f32f36e209b33f50d82d49
Vincent Lohse <vincent.lohse@live.de>
2020-06-29 12:20:44 +0200

    Basic Formatting Main.scala

--- cli/src/swam/cli/Main.scala
+++ cli/src/swam/cli/Main.scala
@@ -116,150 +116,153 @@ object Main extends CommandIOApp(name = "swam-cli", header = "Swam from the comm
     (wasmFile, out, debug).mapN(Compile(_, _, _))
   }
 
-  val serverOpts: Opts[Options] = Opts.subcommand("run_server", "Run a TCP server for a given WASM that listens to inputs") {
-    // TODO: Check which ones of these are really necessary
-    (mainFun, wat, wasi, time, dirs, trace, traceFile, filter, debug, wasmFile, restArguments, cov, out).mapN {
-      (main, wat, wasi, time, dirs, trace, traceFile, filter, debug, wasm, args, cov, out) =>
-        RunServer(wasm, args, main, wat, wasi, time, trace, filter, traceFile, dirs, debug, cov, out)
+  val serverOpts: Opts[Options] =
+    Opts.subcommand("run_server", "Run a TCP server for a given WASM that listens to inputs") {
+      // TODO: Check which ones of these are really necessary
+      (mainFun, wat, wasi, time, dirs, trace, traceFile, filter, debug, wasmFile, restArguments, cov, out).mapN {
+        (main, wat, wasi, time, dirs, trace, traceFile, filter, debug, wasm, args, cov, out) =>
+          RunServer(wasm, args, main, wat, wasi, time, trace, filter, traceFile, dirs, debug, cov, out)
+      }
     }
-  }
   val outFileOptions = List(StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
 
   def main: Opts[IO[ExitCode]] =
-    runOpts.orElse(serverOpts).orElse(covOpts).orElse(decompileOpts).orElse(validateOpts).orElse(compileOpts).map { opts =>
-      Blocker[IO].use { blocker =>
-        opts match {
-          case Run(file, args, main, wat, wasi, time, trace, filter, tracef, dirs, debug) =>
-            for {
-              tracer <- if (trace)
-                JULTracer[IO](blocker,
-                              traceFolder = ".",
-                              traceNamePattern = tracef.toAbsolutePath().toString(),
-                              filter = filter,
-                              formatter = NoTimestampFormatter).map(Some(_))
-              else
-                IO(None)
-              engine <- Engine[IO](blocker, tracer)
-              tcompiler <- swam.text.Compiler[IO](blocker)
-              module = if (wat) tcompiler.stream(file, debug, blocker) else engine.sections(file, blocker)
-              compiled <- engine.compile(module)
+    runOpts.orElse(serverOpts).orElse(covOpts).orElse(decompileOpts).orElse(validateOpts).orElse(compileOpts).map {
+      opts =>
+        Blocker[IO].use { blocker =>
+          opts match {
+            case Run(file, args, main, wat, wasi, time, trace, filter, tracef, dirs, debug) =>
+              for {
+                tracer <- if (trace)
+                  JULTracer[IO](blocker,
+                                traceFolder = ".",
+                                traceNamePattern = tracef.toAbsolutePath().toString(),
+                                filter = filter,
+                                formatter = NoTimestampFormatter).map(Some(_))
+                else
+                  IO(None)
+                engine <- Engine[IO](blocker, tracer)
+                tcompiler <- swam.text.Compiler[IO](blocker)
+                module = if (wat) tcompiler.stream(file, debug, blocker) else engine.sections(file, blocker)
+                compiled <- engine.compile(module)
 
-              preparedFunction <- prepareFunction(compiled, main, dirs, args, wasi, blocker)
-              _ <- executeFunction(preparedFunction, time)
-            } yield ExitCode.Success
+                preparedFunction <- prepareFunction(compiled, main, dirs, args, wasi, blocker)
+                _ <- executeFunction(preparedFunction, time)
+              } yield ExitCode.Success
 
             // TODO: Delete this and make Option(coverageListener) in .instCoverage()
-          case RunWithCov(file, args, main, wat, wasi, time, trace, filter, tracef, dirs, debug, coverage, out) =>
-            for {
-              tracer <- if (trace)
-                JULTracer[IO](blocker,
-                  traceFolder = ".",
-                  traceNamePattern = tracef.toAbsolutePath().toString(),
-                  filter = filter,
-                  formatter = NoTimestampFormatter).map(Some(_))
-              else
-                IO(None)
-              coverageListener = CoverageListener[IO]()
-              engine <- Engine[IO](blocker, tracer, listener = Option(coverageListener))
-              tcompiler <- swam.text.Compiler[IO](blocker)
-              module = if (wat) tcompiler.stream(file, debug, blocker) else engine.sections(file, blocker)
-              compiled <- engine.compile(module)
-              preparedFunction <- prepareFunction(compiled, main, dirs, args, wasi, blocker)
-              _ <- executeFunction(preparedFunction, time)
-              _ <- if (coverage) IO(CoverageReporter.instCoverage(Option(out), file, coverageListener, coverage)) else IO(None)
-            } yield ExitCode.Success
+            case RunWithCov(file, args, main, wat, wasi, time, trace, filter, tracef, dirs, debug, coverage, out) =>
+              for {
+                tracer <- if (trace)
+                  JULTracer[IO](blocker,
+                                traceFolder = ".",
+                                traceNamePattern = tracef.toAbsolutePath().toString(),
+                                filter = filter,
+                                formatter = NoTimestampFormatter).map(Some(_))
+                else
+                  IO(None)
+                coverageListener = CoverageListener[IO]()
+                engine <- Engine[IO](blocker, tracer, listener = Option(coverageListener))
+                tcompiler <- swam.text.Compiler[IO](blocker)
+                module = if (wat) tcompiler.stream(file, debug, blocker) else engine.sections(file, blocker)
+                compiled <- engine.compile(module)
+                preparedFunction <- prepareFunction(compiled, main, dirs, args, wasi, blocker)
+                _ <- executeFunction(preparedFunction, time)
+                _ <- if (coverage) IO(CoverageReporter.instCoverage(Option(out), file, coverageListener, coverage))
+                else IO(None)
+              } yield ExitCode.Success
 
-          case RunServer(file, args, main, wat, wasi, time, trace, filter, tracef, dirs, debug, coverage, out) =>
-            for {
-              tracer <- if (trace)
-                JULTracer[IO](blocker,
-                  traceFolder = ".",
-                  traceNamePattern = tracef.toAbsolutePath().toString(),
-                  filter = filter,
-                  formatter = NoTimestampFormatter).map(Some(_))
-              else
-                IO(None)
-              coverageListener = CoverageListener[IO]()
-              engine <- Engine[IO](blocker, tracer, listener = Option(coverageListener))
-              tcompiler <- swam.text.Compiler[IO](blocker)
-              module = if (wat) tcompiler.stream(file, debug, blocker) else engine.sections(file, blocker)
-              compiled <- engine.compile(module)
+            case RunServer(file, args, main, wat, wasi, time, trace, filter, tracef, dirs, debug, coverage, out) =>
+              for {
+                tracer <- if (trace)
+                  JULTracer[IO](blocker,
+                                traceFolder = ".",
+                                traceNamePattern = tracef.toAbsolutePath().toString(),
+                                filter = filter,
+                                formatter = NoTimestampFormatter).map(Some(_))
+                else
+                  IO(None)
+                coverageListener = CoverageListener[IO]()
+                engine <- Engine[IO](blocker, tracer, listener = Option(coverageListener))
+                tcompiler <- swam.text.Compiler[IO](blocker)
+                module = if (wat) tcompiler.stream(file, debug, blocker) else engine.sections(file, blocker)
+                compiled <- engine.compile(module)
 
-              preparedFunction <- prepareFunction(compiled, main, dirs, args, wasi, blocker)
-              _ <- IO(Server.listen(preparedFunction, time, Option(out), file, coverageListener, coverage))
+                preparedFunction <- prepareFunction(compiled, main, dirs, args, wasi, blocker)
+                _ <- IO(Server.listen(preparedFunction, time, Option(out), file, coverageListener, coverage))
 
-            } yield ExitCode.Success
+              } yield ExitCode.Success
 
-          case Decompile(file, textual, out) =>
-            for {
-              decompiler <- if (textual)
-                TextDecompiler[IO](blocker)
-              else
-                RawDecompiler[IO]
-              doc <- decompiler.decompilePath(file, blocker)
-              outPipe = out.fold(fs2.io.stdout[IO](blocker))(fs2.io.file.writeAll[IO](_, blocker, outFileOptions))
-              _ <- Stream
-                .emits(doc.render(10).getBytes("utf-8"))
-                .through(outPipe)
-                .compile
-                .drain
-            } yield ExitCode.Success
+            case Decompile(file, textual, out) =>
+              for {
+                decompiler <- if (textual)
+                  TextDecompiler[IO](blocker)
+                else
+                  RawDecompiler[IO]
+                doc <- decompiler.decompilePath(file, blocker)
+                outPipe = out.fold(fs2.io.stdout[IO](blocker))(fs2.io.file.writeAll[IO](_, blocker, outFileOptions))
+                _ <- Stream
+                  .emits(doc.render(10).getBytes("utf-8"))
+                  .through(outPipe)
+                  .compile
+                  .drain
+              } yield ExitCode.Success
 
-          case Validate(file, wat, dev) =>
-            val throwableFormat =
-              if (dev)
-                ThrowableFormat(ThrowableFormat.Depth.Full, ThrowableFormat.Indent.Fixed(2))
-              else
-                ThrowableFormat(ThrowableFormat.Depth.Fixed(0), ThrowableFormat.Indent.NoIndent)
-            val formatter = Formatter.create(throwableFormat, true)
-            val logger = consoleLogger[IO](formatter = formatter)
-            for {
-              engine <- Engine[IO](blocker)
-              tcompiler <- swam.text.Compiler[IO](blocker)
-              module = if (wat) tcompiler.stream(file, false, blocker) else engine.sections(file, blocker)
-              res <- engine.validate(module).attempt
-              _ <- res.fold(t => logger.error("Module is invalid", t), _ => logger.info("Module is valid"))
-            } yield ExitCode.Success
+            case Validate(file, wat, dev) =>
+              val throwableFormat =
+                if (dev)
+                  ThrowableFormat(ThrowableFormat.Depth.Full, ThrowableFormat.Indent.Fixed(2))
+                else
+                  ThrowableFormat(ThrowableFormat.Depth.Fixed(0), ThrowableFormat.Indent.NoIndent)
+              val formatter = Formatter.create(throwableFormat, true)
+              val logger = consoleLogger[IO](formatter = formatter)
+              for {
+                engine <- Engine[IO](blocker)
+                tcompiler <- swam.text.Compiler[IO](blocker)
+                module = if (wat) tcompiler.stream(file, false, blocker) else engine.sections(file, blocker)
+                res <- engine.validate(module).attempt
+                _ <- res.fold(t => logger.error("Module is invalid", t), _ => logger.info("Module is valid"))
+              } yield ExitCode.Success
 
-          case Compile(file, out, debug) =>
-            for {
-              tcompiler <- swam.text.Compiler[IO](blocker)
-              _ <- (Stream.emits(ModuleStream.header.toArray) ++ tcompiler
-                .stream(file, debug, blocker)
-                .through(ModuleStream.encoder.encode[IO])
-                .flatMap(bv => Stream.emits(bv.toByteArray)))
-                .through(fs2.io.file.writeAll(out, blocker, outFileOptions))
-                .compile
-                .drain
-            } yield ExitCode.Success
+            case Compile(file, out, debug) =>
+              for {
+                tcompiler <- swam.text.Compiler[IO](blocker)
+                _ <- (Stream.emits(ModuleStream.header.toArray) ++ tcompiler
+                  .stream(file, debug, blocker)
+                  .through(ModuleStream.encoder.encode[IO])
+                  .flatMap(bv => Stream.emits(bv.toByteArray)))
+                  .through(fs2.io.file.writeAll(out, blocker, outFileOptions))
+                  .compile
+                  .drain
+              } yield ExitCode.Success
 
-        }
-      }
-    }
-
-    def prepareFunction(module: Module[IO],
-            function_name: String,
-            preopenedDirs: List[Path],
-            args: List[String],
-            useWasi: Boolean,
-            blocker: Blocker): IO[() => IO[Unit]] = {
-        val logger = consoleLogger[IO]()
-        if (useWasi) {
-          Wasi[IO](preopenedDirs, function_name :: args, logger, blocker).use { wasi =>
-            for {
-                instance <- module.importing("wasi_snapshot_preview1", wasi).instantiate
-                exportedFunc <- instance.exports.typed.function[Unit, Unit](function_name)
-                memory <- instance.exports.memory("memory")
-                _ <- wasi.mem.complete(memory)
-            } yield exportedFunc
           }
-        } else {
-            for {
-                instance <- module.instantiate
-                exportedFunc <- instance.exports.typed.function[Unit, Unit](function_name)
-            } yield exportedFunc
         }
     }
+
+  def prepareFunction(module: Module[IO],
+                      function_name: String,
+                      preopenedDirs: List[Path],
+                      args: List[String],
+                      useWasi: Boolean,
+                      blocker: Blocker): IO[() => IO[Unit]] = {
+    val logger = consoleLogger[IO]()
+    if (useWasi) {
+      Wasi[IO](preopenedDirs, function_name :: args, logger, blocker).use { wasi =>
+        for {
+          instance <- module.importing("wasi_snapshot_preview1", wasi).instantiate
+          exportedFunc <- instance.exports.typed.function[Unit, Unit](function_name)
+          memory <- instance.exports.memory("memory")
+          _ <- wasi.mem.complete(memory)
+        } yield exportedFunc
+      }
+    } else {
+      for {
+        instance <- module.instantiate
+        exportedFunc <- instance.exports.typed.function[Unit, Unit](function_name)
+      } yield exportedFunc
+    }
+  }
 
   def executeFunction(preparedFunction: () => IO[Unit], time: Boolean): IO[Unit] = {
     val logger = consoleLogger[IO]()
