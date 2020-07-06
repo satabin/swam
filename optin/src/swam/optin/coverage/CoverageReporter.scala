f81b75e486a53869f1398775dce69f8c6d98da7f
Javier Cabrera <javierca@kth.se>
2020-07-06 14:55:00 +0200

    WIP: patching permissions

--- optin/src/swam/optin/coverage/CoverageReporter.scala
+++ optin/src/swam/optin/coverage/CoverageReporter.scala
@@ -47,7 +47,6 @@ object CoverageReporter {
 
   private def logCoverage(dir: Any, watOrWasm: Path, list: List[ModuleCoverageInfo]): Unit = {
 
-    println("Covering")
     implicit val modEncoder: HeaderEncoder[ModuleCoverageInfo] =
       HeaderEncoder.caseEncoder("Method Name", "Covered Instruction", "Total Instruction")(ModuleCoverageInfo.unapply _)
 
@@ -79,7 +78,6 @@ object CoverageReporter {
   def instCoverage(dir: Any, watOrWasm: Path, instance: CoverageListener[IO], logOrNot: Boolean) = {
     val list = buildCoverage(instance)
     //if (logOrNot)
-    println("Reporting")
     logCoverage(dir, watOrWasm, list)
   }
 }
